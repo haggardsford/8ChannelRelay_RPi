aa811e04a4a104045097c2cc0f5ca344008e8270
Jordan Hansford <hansfordjordan@gmail.com>
2015-06-30 09:50:11 -0400

    Update RELAY.py
    comments, full pinList initialized on R.pinInit() call

--- RELAY.py
+++ RELAY.py
@@ -1,36 +1,40 @@
 #!/usr/bin/python
 import RPi.GPIO as GPIO
-global pinList
-pinList = [2,3,4,17,27,22,10,9]
+global pinList  
+pinList = [2,3,4,17,27,22,10,9]                #list of pins used for switching relays(using 8-channel relay board)
 
-def switchOn(relay):
+def pinInit():                   
+   GPIO.setmode(GPIO.BCM)                       #sets GPIO to BCM mode
+   for i in range(len(pinList)):                #initializes all pins in pinList to outputs, HIGH = relay off
+       GPIO.setup(pinList[i], GPIO.OUT, \
+       initial=GPIO.HIGH) 
+
+def switchOn(relay):                             # switches relay on if off, otherwise returns False
     if relay.isOn() == False:
-	relay.switchRelay()
+        relay.switchRelay()
     else: return False
 
-def switchOff(relay):
+def switchOff(relay):                            # switches relay off if on, otherwise returns false
     if relay.isOn() == True:
-	relay.switchRelay()
+    	relay.switchRelay()
     else: return False
 
-def pinInit():
-   GPIO.setmode(GPIO.BCM)    #init PIN mode
-   for i in range(len(pinList)):
-   	GPIO.setup(pinList[i], GPIO.OUT, initial=GPIO.HIGH) 
+
 
 class Relay:
    
-    def __init__(self, relay):
+    def __init__(self, relay):                   #set relay 1-8 to i in pinList 0-7, so there isn't a relay 0  
         self.relay = relay - 1   
-    
-    def isOn(self):
+        
+    def isOn(self):                              #checks whether a relay is on, returns True or False
     	if GPIO.input(pinList[self.relay]) == 1:
             return False
     	else: return True
 
-    def switchRelay(self):
+    def switchRelay(self):                       #switches relay state
        	
-    	GPIO.output(pinList[self.relay], not GPIO.input(pinList[self.relay]))
+    	GPIO.output(pinList[self.relay],\
+    	not GPIO.input(pinList[self.relay]))
               
 
     	
