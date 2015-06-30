aaa9027b2c5cbaa459442d0e354552cc0c18329a
Jordan Hansford <hansfordjordan@gmail.com>
2015-06-30 11:21:33 -0400

    Update relay.py
    included set_pinList method, test to see if overrides default pinList (by making is not include some pins perhaps)

--- relay.py
+++ relay.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python
 import RPi.GPIO as GPIO
-global pinList  
-pinList = [2,3,4,17,27,22,10,9]                #list of pins used for switching relays(using 8-channel relay board)
+global pinList
+pinList = (2, 3, 4, 17, 27, 22, 10, 9)
+def set_pinList(*args):                         #sets pinList, the list of pins to be initialized for use as relays
+    global pinList                              #pinList should be between 1 and 8 ints long, ex:(1) - (1,2,3,4,5,6,7,8) BCM pin numbering  
+    pinList = args 
 
 def pinInit():                   
    GPIO.setmode(GPIO.BCM)                       #sets GPIO to BCM mode
-   for i in range(len(pinList)):                #initializes all pins in pinList to outputs, HIGH = relay off
-       GPIO.setup(pinList[i], GPIO.OUT, \
+   for i in pinList:                #initializes all pins in pinList to outputs, HIGH = relay off
+       GPIO.setup(i, GPIO.OUT, \
        initial=GPIO.HIGH) 
 
 def switchOn(relay):                             # switches relay on if off, otherwise returns False
