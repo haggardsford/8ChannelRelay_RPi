5b3bedacfc98c59019ba0f1eeeece22bcc10d91a
Jordan Hansford <hansfordjordan@gmail.com>
2015-07-15 10:44:04 -0400

    Update relaylogic.py

--- relaylogic.py
+++ relaylogic.py
@@ -7,3 +7,11 @@
 #Relay 4 Cooling
 
 
+    #connect to database, create cursor
+        db = sqlite3.connect('dht.db')
+        c = db.cursor()
+        data =  c.execute('''SELECT * FROM sensordata WHERE ID = (SELECT MAX(ID) FROM sensordata''')
+        
+        db.commit()
+        db.close()
+
