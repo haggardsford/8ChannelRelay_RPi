f36ecbd51f4d60dff1a3b04d7bde164852b0bf1a
Jordan Hansford <hansfordjordan@gmail.com>
2015-07-15 14:57:05 -0400

    Update relaylogic.py

--- relaylogic.py
+++ relaylogic.py
@@ -7,7 +7,9 @@
 #Relay 4 Cooling
 import sqlite3
 import relay as R
+import pid
 from datetime import datetime
+from time import sleep
 
     #connect to database, create cursor
 def get_cdata():
@@ -31,6 +33,52 @@ def get_chour():
     now = datetime.now()
     return now.hour
 
+def set_pid(name, P, I, D):
+    name = PID(P, I, D)
+
+def set_setPoint(name, SP):
+    global setpoint
+    setpoint = SP
+    name.setPoint(SP)
+    return SP
+    
+def get_setPoint():
+    return setpoint
+
+def get_Heat_Output(temp):
+    heat_output = temp_controller(temp)
+    if heat_output < 0: 
+        return 0 
+    elif heat_output > 100:   
+        return 100 
+    else: return heat_output   
+
+'''TAKEN FROM RasPiBrew!!!'''
+'''Edit this to work with dht and relays'''
+def heat_Process(cycle_time, duty_cycle, relay):
+    p = current_process()
+    print 'Starting:', p.name, p.pid
+    if pinNum > 0:
+        GPIO.setup(pinNum, GPIO.OUT)
+        while (True):
+            while (conn.poll()): #get last
+                cycle_time, duty_cycle = conn.recv()
+            conn.send([cycle_time, duty_cycle])  
+            if duty_cycle == 0:
+                GPIO.output(pinNum, OFF)
+                time.sleep(cycle_time)
+            elif duty_cycle == 100:
+                GPIO.output(pinNum, ON)
+                time.sleep(cycle_time)
+            else:
+                on_time, off_time = getonofftime(cycle_time, duty_cycle)
+                GPIO.output(pinNum, ON)
+                time.sleep(on_time)
+                GPIO.output(pinNum, OFF)
+                time.sleep(off_time)
+
+
+
 
 
 def main():
@@ -40,6 +88,7 @@ def main():
     humR = R.Relay(3)
     coolR = R.Relay(4)
     
+
     while True:
         temp = get_ctemp()
         hum = get_chum()
