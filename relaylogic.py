74e882652c7de1abc8aead2c0e69c269d40fe513
Haggardsford <jordan@localhost.localdomain>
2015-07-17 09:55:11 -0400

    test

--- relaylogic.py
+++ relaylogic.py
@@ -1,6 +1,6 @@
 #Script that pulls latest temp and humidity data from the dht.db sensordata table 
 #to be used to drive relays On or OFF
-
+#TESTTESTTEST Push to GIT
 #Relay 1 Lights
 #Relay 2 Heat
 #Relay 3 Humidity
